7f11ffa648b5afa4c32839dc86bd3d8b9ef4cb79
Roman6753 <167601689+Roman6753@users.noreply.github.com>
2024-05-07 10:22:50 +0500

    Add files via upload

---
+++ js/compania.js
@@ -0,0 +1,8 @@
+const userIcon = document.querySelector('.user');
+const menu = document.querySelector('.menu');
+
+const Menu = () => {
+  menu.classList.toggle('visible');
+};
+
+userIcon.addEventListener('click', Menu);
